0d85f3c21e6dfac5f97b5024dba7d77af905fa1f
Koichiro IWAO <meta@vmeta.jp>
2015-03-27 17:43:29 +0900

    Fix xorgxrdp build, resolves #6, resolves #8
    Follow upstream change. Now xorg driver is provided as git submodule.

--- X11RDP-RH-Matic.sh
+++ X11RDP-RH-Matic.sh
@@ -170,11 +170,12 @@ fetch()
 	WRKSRC=${GH_ACCOUNT}-${GH_PROJECT}-${GH_COMMIT}
 	DISTFILE=${WRKSRC}.tar.gz
 	echo -n 'Fetching source code... '
+	
 	if [ ! -f ${SOURCE_DIR}/${DISTFILE} ]; then
-		wget \
-			--quiet \
-			--output-document=${SOURCE_DIR}/${DISTFILE} \
-			https://codeload.github.com/${GH_ACCOUNT}/${GH_PROJECT}/legacy.tar.gz/${GH_COMMIT} && \
+		git clone --recursive ${GH_URL} ${WRKDIR}/${WRKSRC} >> $BUILD_LOG 2>&1 && \
+		tar cfz ${WRKDIR}/${DISTFILE} -C ${WRKDIR} ${WRKSRC} && \
+		cp -a ${WRKDIR}/${DISTFILE} ${SOURCE_DIR}/${DISTFILE} || error_exit
+
 		echo 'done'
 	else
 		echo 'already exists'
