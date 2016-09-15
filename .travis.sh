a8f3a874394fcca887249216d8142179867ac49f
Koichiro IWAO <meta@vmeta.jp>
2016-09-15 14:07:52 +0900

    CI: add .travis.yml

---
+++ .travis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ARGS=$@
+
+yum install -y sudo git
+sed -i.bak \
+  -e 's/\(^%wheel\s*ALL=(ALL)\s*ALL\)/# \1/' \
+  -e 's/^#\s\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' \
+  /etc/sudoers
+useradd -m -G wheel travis
+# Docker issue #2259
+chown -R travis:travis ~travis
+sudo -u travis ./X11RDP-RH-Matic.sh ${ARGS}
