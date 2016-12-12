4e1e5bdfa3d80b30a7b2384ee9d931e57abe085c
Koichiro IWAO <meta@vmeta.jp>
2016-12-12 14:49:11 +0900

    Remove useless patches from EXTRA_SOURCE

--- X11RDP-RH-Matic.sh
+++ X11RDP-RH-Matic.sh
@@ -52,7 +52,7 @@ SOURCE_DIR=$(rpm --eval %{_sourcedir})
 TARGETS="xrdp x11rdp"
 META_DEPENDS="rpm-build rpmdevtools"
 FETCH_DEPENDS="ca-certificates git wget"
-EXTRA_SOURCE="xrdp.init xrdp.sysconfig xrdp.logrotate xrdp-pam-auth.patch buildx_patch.diff x11_file_list.patch sesman.ini.master.patch sesman.ini.devel.patch"
+EXTRA_SOURCE="xrdp.init xrdp.sysconfig xrdp.logrotate xrdp-pam-auth.patch buildx_patch.diff x11_file_list.patch sesman.ini.master.patch"
 XRDP_CONFIGURE_ARGS="--enable-fuse --enable-jpeg --disable-static"
 
 # flags
